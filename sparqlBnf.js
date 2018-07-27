77fcaee46fa19e6c5816aa8a2c6415d4af3d340c
PierreLabs <pierre.bournerie@outlook.com>
2018-07-27 14:25:37 +0200

    premier commit

---
+++ sparqlBnf.js
@@ -0,0 +1,235 @@
+/*jshint esversion: 6 */
+$(function() {
+
+    var uri = document.getElementById("uri").value;
+    document.getElementById('btn').addEventListener("click", function() {
+        sparqlData(uri);
+    });
+
+
+
+
+    function sparqlData(uri) {
+
+        var nodes = []; //Les noeuds
+        var links = []; //Les arcs
+        var dataobj = {}; //Objet des tableaux noeuds/liens
+
+        //http://data.bnf.fr/ark:/12148/cb11907966z
+        //http://data.bnf.fr/ark:/12148/cb14793455w Giuliani
+        var endpoint = "http://data.bnf.fr/sparql";
+        var req = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> PREFIX dcterms: <http://purl.org/dc/terms/> SELECT DISTINCT ?work ?title ?nom WHERE {<" + uri + "> foaf:focus ?person ;<http://www.w3.org/2004/02/skos/core#prefLabel> ?nom . ?work dcterms:creator ?person; rdfs:label ?title .} LIMIT 100";
+
+        $.ajax({
+            url: endpoint,
+            dataType: 'json',
+            data: {
+                queryLn: 'SPARQL',
+                query: req,
+                limit: 'none',
+                infer: 'true',
+                Accept: 'application/sparql-results+json'
+            },
+            success: graphResultat,
+            error: displayError
+        });
+
+        function displayError(xhr, textStatus, errorThrown) {
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+
+        function graphResultat(oeuvres) {
+            $.each(oeuvres.results.bindings, function(i, oeuvre) {
+                if (i === 0) {
+                    //nodes index 0 = auteur
+                    nodes.push({ id: oeuvre.nom.value, uri: uri, group: "auteur" });
+                    nodes.push({ id: oeuvre.title.value, uri: oeuvre.work.value, group: "oeuvre" });
+                } else {
+                    nodes.push({ id: oeuvre.title.value, uri: oeuvre.work.value, group: "oeuvre" });
+                }
+                links.push({ target: oeuvre.nom.value, source: oeuvre.title.value, value: "Créateur" });
+            });
+            var newnodes = supprDoublons(nodes, "id"); //Tableau des noeuds uniques
+            dataobj = {
+                nodes: newnodes,
+                links: links
+            };
+
+            //Init D3
+            d3.selectAll("svg > *").remove();
+
+            var svg = d3.select("svg"),
+                width = $("#lesvg").width(), //+svg.attr("width"),
+                height = $("#lesvg").height(); //+svg.attr("height");
+
+            var tabcouleurs = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+            var color = d3.scaleOrdinal(tabcouleurs); //d3.schemeCategory10
+
+            var g = svg.append("g");
+
+            //Zoom
+            var zoom = d3.zoom()
+                .scaleExtent([0.8 / 2, 4])
+                .on("zoom", zoomed);
+
+            svg.call(zoom);
+
+            function zoomed() {
+                g.attr("transform", d3.event.transform);
+            }
+
+            //Mise en place des forces
+            var simulation = d3.forceSimulation()
+                .force("link", d3.forceLink().id(function(d) {
+                    return d.id;
+                }).distance(function(d) {
+                    //évalue la longueur du lien en fonction de la longueur de chaine
+                    return d.value.length + 2 > 5 ? d.value.length * 10 : 50;
+                }))
+                .force("charge", d3.forceManyBody())
+                .force("center", d3.forceCenter(width / 2, height / 2));
+
+            //liens
+            var link = g
+                .attr("class", "links")
+                .selectAll("line")
+                .data(dataobj.links)
+                .enter().append("line")
+                .attr("stroke-width", 1)
+                .attr("stroke", function(d) { return color(d.value); });
+
+            //Chemins labels
+            var pathT = g.selectAll(".links")
+                .data(dataobj.links)
+                .enter().append("path")
+                .attr("class", "pathT")
+                .attr("id",
+                    function(d) {
+                        return "path" + d.source + "_" + d.target;
+                    });
+
+            //Labels
+            var label = g.selectAll("text")
+                .data(dataobj.links)
+                .enter().append("text");
+
+            label
+                .style("font", "normal 11px Arial")
+                .style("fill", function(d) {
+                    d3.selectAll("input").select(function() {
+                        if (this.value == d.value) {
+                            //couleurs inputs (texte pour les arcs)
+                            this.style.color = color(d.value);
+                        }
+                    });
+                    return color(d.value);
+                })
+                .attr("dy", "-5")
+                .attr("dx", "13")
+                .style('text-anchor', 'start')
+                .attr("fill-opacity", 0.75);
+
+            label.append("textPath")
+                .attr("xlink:href",
+                    function(d) {
+                        return "#path" + d.source + "_" + d.target;
+                    })
+                .text(function(d) {
+                    return d.value + " >";
+                });
+
+            function moveto(d) {
+                return "M" + d.target.x + "," + d.target.y;
+            }
+
+            function lineto(d) {
+                return "L" + d.source.x + "," + d.source.y;
+            }
+
+            //Noeuds
+            var node = g
+                .attr("class", "nodes")
+                .selectAll("circle")
+                .data(dataobj.nodes)
+                .enter().append("circle")
+                .attr("r", function(d) { return d.group == "auteur" ? 30 : 10; })
+                .attr("fill", function(d) {
+                    return color(d.id);
+                })
+                .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended));
+
+            //Title pour avoir l'id du noeud au survol
+            node.append("title")
+                .text(function(d) {
+                    return d.id;
+                });
+
+            simulation
+                .nodes(dataobj.nodes)
+                .on("tick", ticked);
+
+            simulation.force("link")
+                .links(dataobj.links);
+
+            //Fonction itération d3
+            function ticked() {
+                link
+                    .attr("x1", function(d) {
+                        return d.source.x;
+                    })
+                    .attr("y1", function(d) {
+                        return d.source.y;
+                    })
+                    .attr("x2", function(d) {
+                        return d.target.x;
+                    })
+                    .attr("y2", function(d) {
+                        return d.target.y;
+                    });
+
+                node
+                    .attr("cx", function(d) {
+                        return d.x;
+                    })
+                    .attr("cy", function(d) {
+                        return d.y;
+                    });
+
+                pathT
+                    .attr("d",
+                        function(d) {
+                            return moveto(d) + lineto(d);
+                        });
+            }
+
+            function dragstarted(d) {
+                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+
+            function dragended(d) {
+                if (!d3.event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+
+        }
+    }
+
+    //Fonction pour supprimer les doublons dans le tableau des noeuds
+    function supprDoublons(myArr, prop) {
+        return myArr.filter((obj, pos, arr) => {
+            return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+        });
+    }
+});
