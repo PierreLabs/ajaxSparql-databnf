213df7a7dd0bcbfa720797b02f361b4ecd52c7be
PierreLabs <pierre.bournerie@outlook.com>
2018-07-30 10:22:16 +0200

    Message initial

--- js/sparqlBnf.js
+++ js/sparqlBnf.js
@@ -14,6 +14,20 @@ $(function() {
         }
     });
 
+    var svg = d3.select("svg"),
+        width = $("#lesvg").width(), //+svg.attr("width"),
+        height = $("#lesvg").height(); //+svg.attr("height");
+
+    svg
+        .append("g")
+        .attr("transform", "translate(" + (width) / 2 + ",200)")
+        .append("text").transition()
+        .style("font-size", "22px")
+        .style("font-family", "Arial")
+        .style("fill", "#6633cc")
+        .attr("text-anchor", "middle")
+        .text("Renseigner un URI ci-dessous puis cliquer sur envoyer.");
+
 
 
     function sparqlData(uri) {
@@ -81,10 +95,6 @@ $(function() {
                 };
 
                 //Init D3
-
-                var svg = d3.select("svg"),
-                    width = $("#lesvg").width(), //+svg.attr("width"),
-                    height = $("#lesvg").height(); //+svg.attr("height");
 
                 var tabcouleurs = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
                 var color = d3.scaleOrdinal(tabcouleurs); //d3.schemeCategory10
